1a55f7378e22712ebe50ecbd7559def6e3fc3e19
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-28 00:22:40 +0100

    Modification de la fonction main pour extraire tous les canaux d'un fichier WAV en utilisant un motif de nom de fichier de sortie, et suppression du fichier mono_test.wav.

--- main.c
+++ main.c
@@ -5,20 +5,14 @@
 int main(int argc, char** argv) {
 	int return_code = EXIT_FAILURE;
 
-	if (argc == 4) {
-		string filename_in, filename_out;
+	if (argc == 3) {
+		string filename_in, filename_out_pattern;
 		char* endptr;
 
 		filename_in = argv[1];
-		filename_out = argv[2];
-		uint16_t chanId = strtoul(argv[3], &endptr, 10);
+		filename_out_pattern = argv[2];
 
-		FILE* fp_in = fopen(filename_in, "r");
-		FILE* fp_out = fopen(filename_out, "w");
-
-		extractChannel(fp_in, fp_out, chanId);
-
-		fclose(fp_in);
+		extractAllChannels(filename_in, filename_out_pattern);
 
 		return_code = EXIT_SUCCESS;
 	}
